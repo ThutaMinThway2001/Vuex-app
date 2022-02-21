9c5c7dddc87823d1d4f630ab8a5eac81f27375b0
Thuta Min Thway <thutaminthway2001@gmail.com>
2022-02-21 17:03:17 +0630

    lesson 4

--- src/store/modules/Todo.js
+++ src/store/modules/Todo.js
@@ -1,9 +1,15 @@
 export default {
     state:{
-        sayName: "Thuta Min Thway"
+        todos: [
+            { title: "title 1", id: 1 },
+            { title: "title 2", id: 2 },
+            { title: "title 3", id: 3 },
+        ],
     },
     getters:{
-
+        myTodos(state){
+            return state.todos;
+        }
     },
     mutations:{
 
